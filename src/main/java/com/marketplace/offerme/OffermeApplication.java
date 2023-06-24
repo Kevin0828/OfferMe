1c50b5a0995bc49062d278e01f2b98022a723ed2
Kevin Fontela <fontelakevin0828@gmail.com>
2023-06-23 23:28:26 -0400

    initial commit

---
+++ src/main/java/com/marketplace/offerme/OffermeApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.offerme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffermeApplication.class, args);
+	}
+
+}
